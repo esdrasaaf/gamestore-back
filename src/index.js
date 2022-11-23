393174ad274cfb6cb874592d909bd97c445d79da
EduuCarvalho <educarvalho.engcivil@gmail.com>
2022-11-23 17:34:34 -0300

    feat: user schema validation rota post signup

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
+
 dotenv.config()
+
+import usersRouters from './routes/users.routes.js'   // PAU TA DANDO NESSA LINHA AKI
 
 const app = express()
 
 //Configs
 app.use(cors())
 app.use(express.json())
+app.use(usersRouters)
+
+
+
+
+
+
 
 const port = process.env.PORT || 5000
 
