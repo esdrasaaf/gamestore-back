2ceec949f8224297883e9429bc0d3d52cdbc8b52
EduuCarvalho <educarvalho.engcivil@gmail.com>
2022-11-23 21:13:59 -0300

    feat: melhorias na validação user schema

--- src/middlewares/schemaUserValidation.js
+++ src/middlewares/schemaUserValidation.js
@@ -5,12 +5,21 @@ export const userSchema = joi.object({
     email:joi.string().required().min(3),
     password:joi.number().required().min(3),
     passwordConfirmation:joi.number().required().min(3),
+   type:joi.string().required().valid("admin","user"),
 });
 
 export function schemaUserValidation(req,res,next){
-    const {name, email, password,passwordConfirmation} = req.body;
+    const {name, email, password,passwordConfirmation,type} = req.body;
 
-    const { error } = userSchema.validate({name, email, password,passwordConfirmation},{abortEarly:false});
+    const user ={
+        name,
+        email,
+        password,
+        passwordConfirmation,
+        type:!type?"user":type,
+    }
+
+    const { error } = userSchema.validate(user,{abortEarly:false});
 
     if (error){
         const errors = error.details.map((detail)=> detail.message);
