dcd35acee30e53cab15570f7ec9674e583b052e2
Esdras Antero <esdrasanterofontes@gmail.com>
2022-11-24 23:40:33 -0300

    feat: selected game route, without middlewares

--- src/controllers/games.controllers.js
+++ src/controllers/games.controllers.js
@@ -1,9 +1,22 @@
+import { ObjectId } from "mongodb";
 import { gamesCollection } from "../database/db.js";
 
 export async function getGames (req, res) {
     try {
         const gameList = await gamesCollection.find().toArray()
         res.send(gameList)
+    } catch (error) {
+        console.log(error)
+        res.sendStatus(500)
+    }
+}
+
+export async function getSelectedGame (req, res) {
+    const { gameId } = req.params
+
+    try {
+        const game = await gamesCollection.findOne({_id: ObjectId(gameId)})
+        res.send(game).status(200)
     } catch (error) {
         console.log(error)
         res.sendStatus(500)
