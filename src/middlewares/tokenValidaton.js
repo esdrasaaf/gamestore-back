156c61363a2800e0c96aef985c4ce5bc761c61b7
Esdras Antero <esdrasanterofontes@gmail.com>
2022-11-26 23:36:41 -0300

    feat: tokenValidation middleware

---
+++ src/middlewares/tokenValidaton.js
@@ -0,0 +1,17 @@
+import { sessionsCollection } from "../database/db.js"
+
+export default async function tokenValidationMD (req, res, next) {
+    const { authorization } = req.headers
+    const token = authorization.replace("Bearer ", "")
+
+    try {
+        const userExist = await sessionsCollection.findOne({token})
+        if (!userExist) { return res.status(401).send("Sua sessão expirou!") }
+        res.sendStatus(200)
+    } catch (error) {
+         console.log(error)
+         res.sendStatus(500)
+    }
+
+    next ()
+}
--- src/routes/games.routes.js
+++ src/routes/games.routes.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import { getGames, getSelectedGame } from "../controllers/games.controllers.js";
+import tokenValidationMD from "../middlewares/tokenValidaton.js";
 
 const router = Router();
+router.use(tokenValidationMD)
 
 router.get("/games", getGames)
 router.get("/games/:gameId", getSelectedGame)
